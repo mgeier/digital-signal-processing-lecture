0805930c22bbd3555f38834249f32b6aecb19f7b
Matthias Geier <Matthias.Geier@gmail.com>
2016-11-19 16:32:59 +0100

    Trying to switch to conda

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@
 import sys
 import os
 
-import sphinx_bootstrap_theme
+#import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -112,25 +112,25 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+#html_theme = 'bootstrap'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    'navbar_title': 'DSP',
-    'navbar_site_name': 'Chapters',
-    'navbar_pagenav_name': 'This Page',
-    'navbar_fixed_top': 'false',
-    'source_link_position': 'none',
-    'bootswatch_theme': 'cosmo',
-    #'bootswatch_theme': 'lumen',
-    #'bootswatch_theme': 'sandstone',
-    #'bootswatch_theme': 'spacelab',
-}
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+## Theme options are theme-specific and customize the look and feel of a theme
+## further.  For a list of options available for each theme, see the
+## documentation.
+#html_theme_options = {
+#    'navbar_title': 'DSP',
+#    'navbar_site_name': 'Chapters',
+#    'navbar_pagenav_name': 'This Page',
+#    'navbar_fixed_top': 'false',
+#    'source_link_position': 'none',
+#    'bootswatch_theme': 'cosmo',
+#    #'bootswatch_theme': 'lumen',
+#    #'bootswatch_theme': 'sandstone',
+#    #'bootswatch_theme': 'spacelab',
+#}
+#
+## Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
