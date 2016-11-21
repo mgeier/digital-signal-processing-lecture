2b26466ced21980daa2210ace55be719a89f516a
Matthias Geier <Matthias.Geier@gmail.com>
2016-11-21 21:12:40 +0100

    WIP save RST

--- conf.py
+++ conf.py
@@ -36,6 +36,8 @@ extensions = [
     'sphinx.ext.mathjax',
 ]
 
+nbsphinx_save_rst = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
